512d8dd024a74399dab8131af5a858a3fd181a4f
shamilkotta <shamilkotta99@gmail.com>
2021-06-17 19:42:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import Hero from './components/Hero/Hero'
+
+function App() {
+  return (
+    <div>
+      <h1>hi</h1>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Hero.scss'
+
+function Hero() {
+    return (
+        <section className="container">
+            <div className="hero">
+                <p className="hero__text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, inventore.
+                </p>
+                <h1 className="hero__title">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis sit labore neque dolor?
+                </h1>
+                <div className="hero__btns">
+                    <button className="hero__btn hero__btn--active">Start Now</button>
+                    <button className="hero__btn">Courses</button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
