b8a45bfc9af24e975102c0395034e3785b78623b
shamilkotta <shamilkotta99@gmail.com>
2021-07-01 16:20:16 +0530

    checkout page created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.scss';
 import Authentication from './pages/Authentication';
 import Cart from './pages/Cart/Cart';
+import Checkout from './pages/Checkout/Checkout';
 import CourseLanding from './pages/Course Landing/CourseLanding';
-// import Home from './pages/Home';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <Authentication/> */}
       {/* <Home/> */}
       {/* <CourseLanding/> */}
-      <Cart/>
+      {/* <Cart/> */}
+      <Checkout/>
     </div>
   );
 }
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -5,18 +5,18 @@ function Cart() {
     return (
         <div className="cart">
             <div className="cart__checkout-group">
-                <p className="cart-checkout__title">Total</p>
+                <p className="cart-checkout__title h">Total</p>
                 <h1 className="cart-checkout__price">$00,000</h1>
                 <button className="cart-checkout__btn btn btn--active">Checkout</button>
             </div>
             <div className="cart__items">
                 <div className="cart__items--active">
-                    <p className="cart__head">00 Courses in Cart</p>
+                    <p className="cart__head p">00 Courses in Cart</p>
                     
                     
                 </div>
                 <div className="cart__items--later">
-                    <p className="cart__head">Saved for later</p>
+                    <p className="cart__head p">saved for later</p>
 
                 </div>
             </div>
